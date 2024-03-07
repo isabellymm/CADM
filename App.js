fe9162f4ac3745d097d1e3a6d45aa4923bf939c6
Isabelly Martins <isabellymartinsmota6@gmail.com>
2024-03-07 08:44:14 -0300

    Update App.js

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ export default function App() {
             resizeMode: "cover",
             backgroundColor: '#fff'
           }}
-            source={require('../my-app/assets/profile.png')}>
+            source={require('../CADM/assets/profile.png')}>
           </Image>
 
           <Pressable style={{ backgroundColor: "#10BC4C", padding: 10, borderRadius: 30, marginRight: 3 }}>
@@ -69,7 +69,7 @@ export default function App() {
             borderRadius: 4,
             elevation: 3
           }}>
-            <Image style={{ width: 55, height: 55 }} source={require('../my-app/assets/flower.jpg')}></Image>
+            <Image style={{ width: 55, height: 55 }} source={require('../CADM/assets/flower.jpg')}></Image>
             <View>
               <Text style={{ fontSize: 15, color: "white" }}>Aleatorio</Text>
             </View>
@@ -91,7 +91,7 @@ export default function App() {
               elevation: 3
             }}>
             <LinearGradient colors={["#33006F", "#FFFFFF"]}>
-              <Image style={{ width: 55, height: 55 }} source={require('../my-app/assets/daily-mix.jpeg')}></Image>
+              <Image style={{ width: 55, height: 55 }} source={require('../CADM/assets/daily-mix.jpeg')}></Image>
             </LinearGradient>
 
             <Text style={{ color: "white", fontSize: 13 }}>Daily Mix 1</Text>
@@ -109,7 +109,7 @@ export default function App() {
             borderRadius: 4,
             elevation: 3
           }}>
-            <Image style={{ width: 55, height: 55 }} source={require('../my-app/assets/top-50.jpg')}></Image>
+            <Image style={{ width: 55, height: 55 }} source={require('../CADM/assets/top-50.jpg')}></Image>
             <View>
               <Text style={{ fontSize: 15, color: "white" }}>Top 50 - Brasil</Text>
             </View>
@@ -131,7 +131,7 @@ export default function App() {
               elevation: 3
             }}>
             <LinearGradient colors={["#33006F", "#FFFFFF"]}>
-              <Image style={{ width: 55, height: 55 }} source={require('../my-app/assets/mega-hiz-mix.jpg')}></Image>
+              <Image style={{ width: 55, height: 55 }} source={require('../CADM/assets/mega-hiz-mix.jpg')}></Image>
             </LinearGradient>
 
             <Text style={{ color: "white", fontSize: 13 }}>Mega Hit Mix</Text>
@@ -149,7 +149,7 @@ export default function App() {
             borderRadius: 4,
             elevation: 3
           }}>
-            <Image style={{ width: 55, height: 55 }} source={require('../my-app/assets/drake-Views.jpg')}></Image>
+            <Image style={{ width: 55, height: 55 }} source={require('../CADM/assets/drake-Views.jpg')}></Image>
             <View>
               <Text style={{ fontSize: 15, color: "white" }}>Views</Text>
             </View>
@@ -171,7 +171,7 @@ export default function App() {
               elevation: 3
             }}>
             <LinearGradient colors={["#33006F", "#FFFFFF"]}>
-              <Image style={{ width: 55, height: 55 }} source={require('../my-app/assets/motomami.jpeg')}></Image>
+              <Image style={{ width: 55, height: 55 }} source={require('../CADM/assets/motomami.jpeg')}></Image>
             </LinearGradient>
 
             <Text style={{ color: "white", fontSize: 13 }}>MOTOMAMI</Text>
@@ -189,7 +189,7 @@ export default function App() {
             borderRadius: 4,
             elevation: 3
           }}>
-            <Image style={{ width: 55, height: 55 }} source={require('../my-app/assets/bin.jpeg')}></Image>
+            <Image style={{ width: 55, height: 55 }} source={require('../CADM/assets/bin.jpeg')}></Image>
             <View>
               <Text style={{ fontSize: 15, color: "white" }}>BIN</Text>
             </View>
@@ -199,22 +199,22 @@ export default function App() {
         <Text style={{ color: "white", fontSize: 23, fontWeight: "bold", marginHorizontal: 10, marginTop: 15 }}>Paradas</Text>
         <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{ padding: 10 }}>
           <View>
-            <Image style={{ width: 120, height: 120, backgroundColor: 'red', borderRadius: 5, marginEnd: 15 }} source={require('../my-app/assets/top-50.jpg')} />
+            <Image style={{ width: 120, height: 120, backgroundColor: 'red', borderRadius: 5, marginEnd: 15 }} source={require('../CADM/assets/top-50.jpg')} />
             <Text style={{ width: 120, height: 30, color: 'white', fontSize: 10, fontWeight: '400', marginTop: 10 }}>Brasil: seu relatório diario das faixas mais tocadas...</Text>
           </View>
 
           <View>
-            <Image style={{ width: 120, height: 120, backgroundColor: 'red', borderRadius: 5, marginEnd: 15 }} source={require('../my-app/assets/top-global.jpeg')} />
+            <Image style={{ width: 120, height: 120, backgroundColor: 'red', borderRadius: 5, marginEnd: 15 }} source={require('../CADM/assets/top-global.jpeg')} />
             <Text style={{ width: 120, height: 30, color: 'white', fontSize: 10, fontWeight: '300', marginTop: 10 }}>Mundo: seu relatório diario das faixas mais tocadas...</Text>
           </View>
 
           <View>
-            <Image style={{ width: 120, height: 120, backgroundColor: 'red', borderRadius: 5, marginEnd: 15 }} source={require('../my-app/assets/top-brasil.jpeg')} />
+            <Image style={{ width: 120, height: 120, backgroundColor: 'red', borderRadius: 5, marginEnd: 15 }} source={require('../CADM/assets/top-brasil.jpeg')} />
             <Text style={{ width: 120, height: 30, color: 'white', fontSize: 10, fontWeight: '300', marginTop: 10 }}>Marshmello, Ana Castela, Mc Ryan SP, Henrique...</Text>
           </View>
 
           <View>
-            <Image style={{ width: 120, height: 120, backgroundColor: 'red', borderRadius: 5, marginEnd: 15 }} source={require('../my-app/assets/viral-sp.jpeg')} />
+            <Image style={{ width: 120, height: 120, backgroundColor: 'red', borderRadius: 5, marginEnd: 15 }} source={require('../CADM/assets/viral-sp.jpeg')} />
             <Text style={{ width: 120, height: 30, color: 'white', fontSize: 10, fontWeight: '300', marginTop: 10 }}>Viral São Paulo BR</Text>
           </View>
         </ScrollView>
@@ -222,22 +222,22 @@ export default function App() {
         <Text style={{ color: "white", fontSize: 23, fontWeight: "bold", marginHorizontal: 10, marginTop: 15 }}>Tocados recentemente</Text>
         <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{ padding: 10 }}>
           <View>
-            <Image style={{ width: 100, height: 100, backgroundColor: 'red', borderRadius: 5, marginEnd: 15, borderRadius: 60 }} source={require('../my-app/assets/the-weeknd.jpeg')} />
+            <Image style={{ width: 100, height: 100, backgroundColor: 'red', borderRadius: 5, marginEnd: 15, borderRadius: 60 }} source={require('../CADM/assets/the-weeknd.jpeg')} />
             <Text style={{ width: 120, height: 20, color: 'white', fontSize: 12, fontWeight: '400', marginTop: 10, marginLeft: 20 }}>The Weeknd</Text>
           </View>
 
           <View>
-            <Image style={{ width: 120, height: 120, backgroundColor: 'red', borderRadius: 5, marginEnd: 15 }} source={require('../my-app/assets/jcole.jpeg')} />
+            <Image style={{ width: 120, height: 120, backgroundColor: 'red', borderRadius: 5, marginEnd: 15 }} source={require('../CADM/assets/jcole.jpeg')} />
             <Text style={{ width: 120, height: 20, color: 'white', fontSize: 12, fontWeight: '400', marginTop: 10 }}>4 Your Eyes Only</Text>
           </View>
 
           <View>
-            <Image style={{ width: 100, height: 100, backgroundColor: 'red', borderRadius: 5, marginEnd: 15, borderRadius: 60 }} source={require('../my-app/assets/new-jeans.jpeg')} />
+            <Image style={{ width: 100, height: 100, backgroundColor: 'red', borderRadius: 5, marginEnd: 15, borderRadius: 60 }} source={require('../CADM/assets/new-jeans.jpeg')} />
             <Text style={{ width: 120, height: 20, color: 'white', fontSize: 12, fontWeight: '400', marginTop: 10, marginLeft: 20 }}>New Jeans</Text>
           </View>
 
           <View>
-            <Image style={{ width: 120, height: 120, backgroundColor: 'red', borderRadius: 5, marginEnd: 15 }} source={require('../my-app/assets/your-right.jpeg')} />
+            <Image style={{ width: 120, height: 120, backgroundColor: 'red', borderRadius: 5, marginEnd: 15 }} source={require('../CADM/assets/your-right.jpeg')} />
             <Text style={{ width: 120, height: 20, color: 'white', fontSize: 12, fontWeight: '400', marginTop: 10 }}>Your Right</Text>
           </View>
         </ScrollView>
@@ -246,28 +246,28 @@ export default function App() {
         <Text style={{ color: "white", fontSize: 23, fontWeight: "bold", marginHorizontal: 10 }}>Seus Programas</Text>
         <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{ padding: 10 }}>
           <View>
-            <Image style={{ width: 120, height: 120, backgroundColor: 'red', borderRadius: 5, marginEnd: 15, borderRadius: 20 }} source={require('../my-app/assets/dev-sem.jpeg')} />
+            <Image style={{ width: 120, height: 120, backgroundColor: 'red', borderRadius: 5, marginEnd: 15, borderRadius: 20 }} source={require('../CADM/assets/dev-sem.jpeg')} />
             <Text style={{ width: 120, height: 15, color: '#10BC4C', fontSize: 10, fontWeight: 'bold', marginTop: 10 }}>Negócios e Tecnologia </Text>
             <Text style={{ width: 120, height: 15, color: 'white', fontSize: 10, fontWeight: '500', marginTop: 2 }}>Dev Sem Fronteiras</Text>
             <Text style={{ width: 120, height: 20, color: 'white', fontSize: 10, fontWeight: '300', marginTop: 2 }}>Show • Alura</Text>
           </View>
 
           <View>
-            <Image style={{ width: 120, height: 120, backgroundColor: 'red', borderRadius: 5, marginEnd: 15, borderRadius: 20 }} source={require('../my-app/assets/images.jpeg')} />
+            <Image style={{ width: 120, height: 120, backgroundColor: 'red', borderRadius: 5, marginEnd: 15, borderRadius: 20 }} source={require('../CADM/assets/images.jpeg')} />
             <Text style={{ width: 120, height: 15, color: '#10BC4C', fontSize: 10, fontWeight: 'bold', marginTop: 10 }}>Humor</Text>
             <Text style={{ width: 120, height: 15, color: 'white', fontSize: 10, fontWeight: '500', marginTop: 2 }}>Suburb Talks</Text>
             <Text style={{ width: 120, height: 20, color: 'white', fontSize: 10, fontWeight: '300', marginTop: 2 }}>Show • Nick Grajeda</Text>
           </View>
 
           <View>
-            <Image style={{ width: 120, height: 120, backgroundColor: 'red', borderRadius: 5, marginEnd: 15, borderRadius: 20 }} source={require('../my-app/assets/ola-gabs.jpeg')} />
+            <Image style={{ width: 120, height: 120, backgroundColor: 'red', borderRadius: 5, marginEnd: 15, borderRadius: 20 }} source={require('../CADM/assets/ola-gabs.jpeg')} />
             <Text style={{ width: 120, height: 15, color: '#10BC4C', fontSize: 10, fontWeight: 'bold', marginTop: 10 }}>Tecnologia </Text>
             <Text style={{ width: 120, height: 15, color: 'white', fontSize: 10, fontWeight: '500', marginTop: 2 }}>Olá Gabs</Text>
             <Text style={{ width: 120, height: 20, color: 'white', fontSize: 10, fontWeight: '300', marginTop: 2 }}>Show • Gabs Ferreira</Text>
           </View>
 
           <View>
-            <Image style={{ width: 120, height: 120, backgroundColor: 'red', borderRadius: 5, marginEnd: 15, borderRadius: 20 }} source={require('../my-app/assets/estamos-bem.jpeg')} />
+            <Image style={{ width: 120, height: 120, backgroundColor: 'red', borderRadius: 5, marginEnd: 15, borderRadius: 20 }} source={require('../CADM/assets/estamos-bem.jpeg')} />
             <Text style={{ width: 120, height: 15, color: '#10BC4C', fontSize: 10, fontWeight: 'bold', marginTop: 10 }}>Estilo de vida e saúde</Text>
             <Text style={{ width: 120, height: 15, color: 'white', fontSize: 10, fontWeight: '500', marginTop: 2 }}>Estamos Bem?</Text>
             <Text style={{ width: 120, height: 20, color: 'white', fontSize: 10, fontWeight: '300', marginTop: 2 }}>Show • Estamos Bem?</Text>
